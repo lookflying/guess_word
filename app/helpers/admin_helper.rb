30438448bd4193669bb2237b7b8208f35f864918
lookflying <lookflying@gmail.com>
2013-12-27 12:31:44 +0800

    add finished activity

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -14,4 +14,8 @@ module AdminHelper
 	def	get_judges_count(user_id)
 		Judge.where(user_id: user_id).count
 	end
+
+	def get_finished_guess_activities_count(user_id)
+		GuessActivity.where(user_id: user_id, status: "finished").count
+	end
 end
