de49530a0c3d12d70e1c76c6961a2f9a92a4f03f
lookflying <lookflying@gmail.com>
2013-12-27 12:49:59 +0800

    forbit recovery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ GuessWord::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+	config.action_mailer.default_url_options = { :host => 'guess.lookflying.com' }
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {:address => "guess.lookflying.com", :port => 1025}
 end
